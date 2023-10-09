61306f67458208d46b1f4a65387eb5cafd84734f
farshadmomo <momtazfarshad@gmail.com>
2023-10-09 17:02:51 +0330

    states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        comments: "",
+        isFriendly: true,
+        employment: "",
+        favColor: ""
+    })
   return (
-      <h1> hello world</h1>
+      <div className='formContainer'>
+        <form className='form'>
+            <input className='form-input' name='firstName'/>
+            <input className='form-input' name='lastName'/>
+            <input className='form-input' name='emailAdress'/>
+            <div className="form-checkbox">
+                <input
+                    id="okayToEmail"
+                    type="checkbox"
+                />
+                <label htmlFor="okayToEmail">I want to join the newsletter</label>
+            </div>
+            <button className='form-submit'>submit</button>
+        </form>
+      </div>
   )
 }
 
